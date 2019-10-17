3a5571f09e18a4b837eb738ba4b4602e900a4ad9
MacKenzie Whalen <mackenzie.whalen@iinteractive.com>
2019-10-17 11:08:21 -0400

    app settings

---
+++ NameThatDog/NameThatDog.Shared/DogName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameThatDog.Shared
+{
+    public class DogName
+    {
+        public string GetDogName()
+        {
+            var dogNames = new List<string> { "Bentley", "Ryder", "Belle", "Tootsie", "Biscuit", "Jazzy"};
+            var random = new Random();
+            int index = random.Next(dogNames.Count);
+
+            return dogNames[index];
+        }
+    }
+}
